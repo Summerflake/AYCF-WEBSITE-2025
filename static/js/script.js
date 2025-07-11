15c4c04b0b5b9f846110c8f73d06eae10006702b
Summerflake <li.xin.yu090321@gmail.com>
2025-07-11 19:43:48 +0800

    disable certain functions for production mode

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,12 @@
-// some javascript here
+// production mode
+window.alert = function () { };
+window.confirm = function () { return false; };
+window.prompt = function () { return null; };
+
+console.log = function () { };
+console.warn = function () { };
+console.error = function () { };
+console.info = function () { };
+console.debug = function () { };
+console.trace = function () { };
+console.table = function () { };
