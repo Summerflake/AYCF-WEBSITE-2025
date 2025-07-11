023edba2e9f94b78cc2dc9dad9ac7c5fb1c51875
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2025-07-11 20:14:19 +0800

    update links

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -4,8 +4,71 @@ const CACHE_NAME = 'aycf-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/offline.html', // Fallback page when offline
+  '/offline.html', // fallback
+  'fenlunti.html',
+  'field_trip.html',
+  'jiaban.html',
+  'organising_team.html',
+  'timeline.html',
+  'resources.html',
+  'robots.txt',
+  'sitemap.xml',
 
+  // TIMELINE JSON FILES
+  'timeline/timeline1.json',
+  'timeline/timeline2.json',
+  'timeline/timeline3.json',
+
+  // STATIC FILES
+
+  'static/css/bootstrap.css',
+  'static/css/bootstrap.min.map',
+  'static/css/style.css',
+  'static/fonts/font.css',
+  'static/js/bootstrap.bundle.min.js',
+  'static/js/boostrap.bundle.min.js.map',
+  'static/js/script.js',
+  'static/js/service-worker.js',
+
+  // IMAGES
+  'static/assets/admin.jpg',
+  'static/assets/clocktower.jpg',
+  'static/assets/concepts.jpg',
+  'static/assets/hci.jpg',
+  'static/assets/hua-logo.png',
+  'static/assets/lixie.webp',
+  'static/assets/logs.jpg',
+  'static/assets/mrboblee.jpg',
+  'static/assets/mrfu.jpg',
+  'static/assets/mslexie.jpg',
+  'static/assets/ntuc.jpg',
+  'static/assets/progs.jpg',
+  'static/assets/pubs.jpg',
+  'static/assets/shengxi.jpg',
+  'static/assets/sph.jpg',
+  'static/assets/sutd.jpg',
+  'static/assets/theme_logo.jpg',
+
+  // TIMELINE IMAGES
+  'static/assets/timeline/1.svg',
+  'static/assets/timeline/2.svg',
+  'static/assets/timeline/3.svg',
+  'static/assets/timeline/4.svg',
+  'static/assets/timeline/5.svg',
+  'static/assets/timeline/6.svg',
+  'static/assets/timeline/7.svg',
+  'static/assets/timeline/8.svg',
+  'static/assets/timeline/9.svg',
+  'static/assets/timeline/10.svg',
+  'static/assets/timeline/11.svg',
+  'static/assets/timeline/12.svg',
+  'static/assets/timeline/13.svg',
+  'static/assets/timeline/14.svg',
+  'static/assets/timeline/15.svg',
+  'static/assets/timeline/16.svg',
+  'static/assets/timeline/17.svg'
+
+//include other filepaths
 ];
 
 self.addEventListener('install', (event) => {
