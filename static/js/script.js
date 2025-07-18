306d00cfa526969c0f5818b4f8fe3fda15d0995e
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2025-07-18 20:14:18 +0800

    remove all forms of caching

--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,43 @@
-// production mode
-/*
-window.alert = function () { };
-window.confirm = function () { return false; };
-window.prompt = function () { return null; };
+// // production mode
+// /*
+// window.alert = function () { };
+// window.confirm = function () { return false; };
+// window.prompt = function () { return null; };
 
-console.log = function () { };
-console.warn = function () { };
-console.error = function () { };
-console.info = function () { };
-console.debug = function () { };
-console.trace = function () { };
-console.table = function () { };
-*/
+// console.log = function () { };
+// console.warn = function () { };
+// console.error = function () { };
+// console.info = function () { };
+// console.debug = function () { };
+// console.trace = function () { };
+// console.table = function () { };
+// */
 
-//activate service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
+// //activate service worker
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then(registration => {
+//         console.log('Service Worker registered with scope:', registration.scope);
+//       })
+//       .catch(error => {
+//         console.error('Service Worker registration failed:', error);
+//       });
+//   });
+// }
 
-function clearCache(){
-  if ('caches' in window) {
-  caches.keys().then(cacheNames => {
-    return Promise.all(
-      cacheNames.map(cacheName => caches.delete(cacheName))
-    );
-  }).then(() => {
-    console.log('All caches deleted.');
-  }).catch(err => {
-    console.error('Error deleting caches:', err);
-  });
-  } else {
-    console.log('Cache API not supported in this browser.');
-  }
-}
+// function clearCache(){
+//   if ('caches' in window) {
+//   caches.keys().then(cacheNames => {
+//     return Promise.all(
+//       cacheNames.map(cacheName => caches.delete(cacheName))
+//     );
+//   }).then(() => {
+//     console.log('All caches deleted.');
+//   }).catch(err => {
+//     console.error('Error deleting caches:', err);
+//   });
+//   } else {
+//     console.log('Cache API not supported in this browser.');
+//   }
+// }
