1f3106dc94a3289021d634eeb555a3932d345e60
Summerflake <li.xin.yu090321@gmail.com>
2025-07-18 19:53:02 +0800

    update service worker scope

--- static/js/script.js
+++ static/js/script.js
@@ -16,7 +16,7 @@ console.table = function () { };
 //activate service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/static/js/service-worker.js') 
+    navigator.serviceWorker.register('/service-worker.js') 
       .then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
