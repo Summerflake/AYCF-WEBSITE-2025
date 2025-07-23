5b3b4689569c3d8f8009a7c25ac50d8ffb9c3f8c
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2025-07-23 12:50:32 +0800

    removed service worker

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,8 +1,8 @@
-// loader.js
-(function() {
-  const script = document.createElement('script');
-  script.src = '/static/js/script.js?v=' + Date.now(); // Prevent caching
-  script.type = 'text/javascript';
-  script.defer = true; // Optional, based on your use case
-  document.head.appendChild(script);
-})();
+// // loader.js
+// (function() {
+//   const script = document.createElement('script');
+//   script.src = '/static/js/script.js?v=' + Date.now(); // Prevent caching
+//   script.type = 'text/javascript';
+//   script.defer = true; // Optional, based on your use case
+//   document.head.appendChild(script);
+// })();
