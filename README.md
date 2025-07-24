464293e75c5c416eac671037695035a9122857be
Summerflake <li.xin.yu090321@gmail.com>
2025-07-24 16:58:51 +0800

    major formatting of code, remove of old code, update sitemap and remove of unnecessary file

---
+++ README.md
@@ -0,0 +1,1 @@
+Created with ❤️ by @Summerflake and @ttiramisu
--- static/js/script.js
+++ static/js/script.js
@@ -10,32 +10,3 @@ console.info = function () { };
 console.debug = function () { };
 console.trace = function () { };
 console.table = function () { };
-
-// //activate service worker
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//       .then(registration => {
-//         console.log('Service Worker registered with scope:', registration.scope);
-//       })
-//       .catch(error => {
-//         console.error('Service Worker registration failed:', error);
-//       });
-//   });
-// }
-
-// function clearCache(){
-//   if ('caches' in window) {
-//   caches.keys().then(cacheNames => {
-//     return Promise.all(
-//       cacheNames.map(cacheName => caches.delete(cacheName))
-//     );
-//   }).then(() => {
-//     console.log('All caches deleted.');
-//   }).catch(err => {
-//     console.error('Error deleting caches:', err);
-//   });
-//   } else {
-//     console.log('Cache API not supported in this browser.');
-//   }
-// }
